48c44a4e9c8512cdc4b0908e5757794613d87b26
dwab <dan.cicconi@gooduncle.com>
2018-02-22 23:37:20 -0500

    Adding smooth scroll on anchor clicks

--- docs/scripts/custom/stuido.js
+++ docs/scripts/custom/stuido.js
@@ -1,8 +1,5 @@
 $(function() {
-  console.log('JS LOADED')
-
-  $( window ).scroll(function() {
-    console.log('scroll')
+  $(window).scroll(function() {
     var scrollTop = $(document).scrollTop()
     if (scrollTop > 450) {
       $('.sticky-nav').addClass('sticky-nav--visible')
@@ -13,5 +10,16 @@ $(function() {
 
   $('.js-games-link').on('click', function() {
     $('.header').toggleClass('header--dropdown')
+  })
+
+  $('a[href*="#"]').click(function(event) {
+
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: target.offset().top }, 1000)
+    }
   })
 })
